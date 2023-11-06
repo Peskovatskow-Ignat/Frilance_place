72dcdae82033ad5658d75238ce9b41e434bf7579
Peskovatskow-Ignat <pesk2305@yandex.ru>
2023-11-06 17:46:46 +0300

    update data base

--- migrations/V1.1.2__create_orders.sql
+++ migrations/V1.1.2__create_orders.sql
@@ -4,12 +4,13 @@ CREATE TABLE orders (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title VARCHAR(100),
     price int,
-    description VARCHAR(200),
+    description text,
     full_description text,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     customer_id INT REFERENCES customer(id),
     skill VARCHAR(100),
     status bool default true
+
 );
 
 COMMIT;
--- migrations/V1.1.4__create_executor_to_order.sql
+++ migrations/V1.1.4__create_executor_to_order.sql
@@ -3,9 +3,9 @@ BEGIN;
 create table executor_to_order (
 
   executor_id int not null references executor(id),
-  order_id int not null  references orders(id),
+  order_id int not null  references orders (id),
   primary key (executor_id, order_id),
-  status bool default false,
+  status VARCHAR(100) default 'sent for approval',
   data TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
